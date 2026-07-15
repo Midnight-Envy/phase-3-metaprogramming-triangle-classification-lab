ce95ce837d3819772144c8d6a125e36d1e8357b3
Thomas Correia <thomas.correiap@gmail.com>
2026-07-15 12:23:00 -0400

    Completed Metaprogramming Triangle Class Lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  class TriangleError < StandardError
+  end
+
+  def initialize(side_one, side_two, side_three)
+    @sides = [side_one, side_two, side_three]
+  end
+
+  def kind
+    raise TriangleError unless valid?
+
+    if @sides.uniq.length == 1
+      :equilateral
+    elsif @sides.uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+private
+
+  def valid?
+    @sides.all? { |side| side > 0 } &&
+      @sides.sort[0] + @sides.sort[1] > @sides.sort[2]
+  end
 end
